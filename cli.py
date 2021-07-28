1384191789eafaac0aa948a64f69985bd761a9c1
k-kumar-01 <kushalkumargupta4@gmail.com>
2021-07-29 03:31:49 +0530

    feat(cli): add save_direct func
    Converts functions with @api decorator to fastpi code
    Creates build folder and saves file there
    Also saves the file with given pathname
    
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>

--- cli.py
+++ cli.py
@@ -58,6 +58,84 @@ def create_fastapi_file(class_name, module_name, apis_list, store_path):
     except FileExistsError:
         raise Exception("The FastAPI file already exists")
 
+
+def methods(cls):
+    return [x for x, y in cls.__dict__.items() if type(y) == FunctionType]
+
+
+def save_direct(
+    path
+):
+    if path[-2:] != "py":
+        raise Exception("the file is not a python file")
+
+    import os
+    import importlib
+    import inspect
+    import sys
+    complete_path = os.path.join(os.getcwd(), path)
+
+    head, module_name = os.path.split(complete_path)
+    sys.path.append(head)
+    module_name = module_name[:-3]
+    try:
+        module = importlib.import_module(module_name)
+    except Exception as e:
+        raise Exception(f"Couldn't import module:", e)
+
+    count = 0
+    class_names = []
+
+    apis_list = []
+    for name, obj in inspect.getmembers(module):
+        try:
+            if isclass(obj):
+                apiDict = dict()
+                count += 1
+                class_names.append(obj.__name__)
+                MysteriousClass = getattr(module, obj.__name__)
+                classMethods = methods(MysteriousClass)
+                for method in classMethods:
+                    method_to_Call = getattr(MysteriousClass, method)
+                    try:
+                        if(hasattr(method_to_Call, 'is_api')):
+                            apiDict['http_methods'] = getattr(
+                                method_to_Call, 'http_methods')
+                            apiDict['route'] = getattr(
+                                method_to_Call, '_api_route')
+                            apiDict['name'] = method
+
+                            apis_list.append(apiDict)
+
+                    except e:
+                        print(e)
+                        pass
+
+        except e:
+            print(e)
+            pass
+    try:
+        buildPath = os.path.join(os.getcwd(),'build')
+        os.mkdir(buildPath)
+    except FileExistsError:
+        raise Exception("The Build Folder already exists. Delete the previous build and re run the command")
+
+    original_file:str
+
+    with open(f"{module_name}.py", "r") as fh:
+        original_file = fh.read()
+    
+    if(len(original_file)>0):
+        try:
+            with open(f'./build/{module_name}.py', "x") as f:
+                f.write(original_file)
+        except FileExistsError:
+            raise Exception("The FastAPI file already exists")
+
+    create_fastapi_file(class_name=module_name,
+                        module_name=class_names[0], apis_list=apis_list, store_path='./build/app.py')
+
+
 def cli():
     parser = argparse.ArgumentParser(
         prog='mylibrary', description='Builds a file for deployment to server')
@@ -68,7 +146,7 @@ def cli():
     args = parser.parse_args()
 
     if args.build:
-        pass
+        save_direct(f'{args.build}')
 
     if args.deploy:
         pass
